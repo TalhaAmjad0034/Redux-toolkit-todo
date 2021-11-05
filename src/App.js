601d9e955acccc1ceba9b55d067760ad90a2cfc0
Talha Amjad <talha.amjad@tecaudex.com>
2021-11-05 08:49:58 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [addTodo, setAddTodo] = useState();
+  const [edit, setEdit] = useState();
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      text: "Learn React",
+    },
+    {
+      id: 2,
+      text: "Learn Redux",
+    },
+    {
+      id: 3,
+      text: "Learn React Native",
+    },
+  ]);
+  const subHandler = () => {
+    setTodo([...todo, { id: todo.length + 1, text: addTodo }]);
+    setAddTodo("");
+  };
+  const deleteHandler = (item) => {
+    setTodo(todo.filter((t) => t.id !== item));
+    console.log(item);
+  };
+  const editHandler = (itemId) => {
+    todo.map((i) => (i.id === itemId ? `${(i.text = edit)}` : i.text));
+    console.log(edit);
+    setTodo([...todo]);
+    setEdit("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Making a counter app using useState</h1>
+      {todo.map((item) => (
+        <div key={item.id}>
+          <p>{item.text}</p>
+          <input onChange={(e) => setEdit(e.target.value)} type="text" />
+          <button onClick={() => editHandler(item.id)}>Edit</button>
+          <button onClick={() => deleteHandler(item.id)}>Delete</button>
+        </div> //key is used to identify the element
+      ))}
+      <br />
+      <input
+        value={addTodo}
+        onChange={(e) => setAddTodo(e.target.value)}
+        type="text"
+      />
+      <button onClick={subHandler}>Add</button>
     </div>
   );
 }
